4430e8ea16c77d58bdad37a5f775d1f970a00eff
Nicolas Baraldo <121053856+NicolasBaraldodeLima@users.noreply.github.com>
2023-10-02 18:01:33 -0300

    Add files via upload

---
+++ array-3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std; 
+
+
+int main() {
+	
+	int tam = 5;
+	int vetor[tam];
+	int i;
+	
+	vetor[0] = 10;
+	vetor[1] = 20;
+	vetor[2] = 30;
+	vetor[3] = 40;
+	vetor[4] = 50;
+	
+	
+	for (i = 0; i < tam; i++) {
+		cout << vetor[i] << "\n";
+	}
+	
+	return 0;
+	
+}
+
+
